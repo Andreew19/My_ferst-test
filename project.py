4b05f0069ab1730539b7fce82eec9dfa40d2b5cb
Andreew19 <andrew_04@list.ru>
2019-12-09 23:23:14 +0400

    lesson

--- project.py
+++ project.py
@@ -1,12 +1,11 @@
 def main(a):
     while True:
-       try:
-           a = int(input("Введите число: "))
-           break
-       except ValueError:
+        try:
+            a = int(input("Введите число: "))
+            break
+        except ValueError:
            print("Это не число")
-       if a == "":
-           print("Здесь пусто")
-       else:
-           print(a)
-main()
+        if a == "":
+           print("Вы не чего не ввели")
+    print(a)
+main(1)
