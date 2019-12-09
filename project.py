288ba29dc97257425c76d840802b8e9f0259349f
Andreew19 <andrew_04@list.ru>
2019-12-09 19:04:02 +0400

    my_comm

--- project.py
+++ project.py
@@ -1,3 +1,12 @@
-a = input("введите число")
-print(a)
-
+def main(a):
+    while True:
+       try:
+           a = int(input("Введите число: "))
+           break
+       except ValueError:
+           print("Это не число")
+       if a == "":
+           print("Здесь пусто")
+       else:
+           print(a)
+main()
