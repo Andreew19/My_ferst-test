194da3a9d9ce40bed9b088848cb38481a78f3267
Andreew19 <andrew_04@list.ru>
2019-12-10 13:06:33 +0400

    fixed

--- project.py
+++ project.py
@@ -2,10 +2,8 @@ def main(a):
     while True:
         try:
             a = int(input("Введите число: "))
-            break
         except ValueError:
            print("Это не число")
-        if a == "":
-           print("Вы не чего не ввели")
-    print(a)
+        c = str(a)
+        print("Хорошое число:"+c)
 main(1)
