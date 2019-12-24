f9ed88a9587a7c5e29565c4c171c79b961b27192
Andreew19 <58325735+Andreew19@users.noreply.github.com>
2019-12-24 21:30:34 +0400

    try

--- project.py
+++ project.py
@@ -1,31 +1,25 @@
 import sys, getopt
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,"hi:",["ifile="])
    except getopt.GetoptError:
-      print ('project.py -i <inputfile> -o <outputfile>') 
-      sys.exit(2)
+      print ('project.py -i <inputfile>') 
+      sys.exit(1)
    for opt, arg in opts:
       if opt == '-h':
-         print ('project.py -i <inputfile> -o <outputfile>') 
+         print ('project.py -i <inputfile>') 
          sys.exit()
       elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print ('Input file is "', inputfile)
-   print ('Output file is "', outputfile)
+       num(arg)
 def num(a):
-    while True:
+
         try:
-            a = int(input("Введите число: "))
+            a = int(input("Введите число:"))
         except ValueError:
            print("Это не число")
         c = str(a)
         print("Хорошое число:"+c)
-num(1)
 if __name__ == "__main__":
-   num(sys.argv[1:])
+   arguments = sys.argv[1:]
+   main(arguments)
