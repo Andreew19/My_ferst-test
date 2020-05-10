418f82811a772cfedcd36ad67170afe9e7d3951c
Andreew19 <58325735+Andreew19@users.noreply.github.com>
2020-05-10 17:44:46 +0400

    Delete project.py

--- project.py
+++
@@ -1,9 +0,0 @@
-def main(a):
-    while True:
-        try:
-            a = int(input("Введите число: "))
-        except ValueError:
-           print("Это не число")
-        c = str(a)
-        print("Хорошое число:"+c)
-main(1)
